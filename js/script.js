7af81189f0610fb22b60eb79e49e752b4a8ff159
Taylor Smith <tjsmith19@gmail.com>
2012-02-25 18:47:01 -0800

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 $(document).ready(function(){
 
-$(".iPhone").append('<div class="time">' + hours + ":" + minutes + " " + suffix + '</div>');
+	$(".iPhone").append('<div class="time">' + hours + ":" + minutes + " " + suffix + '</div>');
+	
+	// $(".anchorLink").anchorAnimate();
+	
 });
 
+// jQuery.fn.anchorAnimate = function(settings) {
+// 
+//  	settings = jQuery.extend({
+// 		speed : 1100
+// 	}, settings);	
+// 	
+// 	return this.each(function(){
+// 		var caller = this
+// 		$(caller).click(function (event) {	
+// 			event.preventDefault()
+// 			var locationHref = window.location.href
+// 			var elementClick = $(caller).attr("href")
+// 			
+// 			var destination = $(elementClick).offset().top;
+// 			$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination}, settings.speed, function() {
+// 				window.location.hash = elementClick
+// 			});
+// 		  	return false;
+// 		})
+// 	})
+// }
 
 var currentTime = new Date()
 var hours = currentTime.getHours()
