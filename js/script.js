8801dbb8348048d8bd4e3792b6b9fd0c9115f9ab
ryan charette <ryan@looprecur.com>
2012-03-01 16:32:13 -0800

    Javascript description popup for single project under work section. Added tooltip_left and tooltip_right classes for the speech bubble triangle depending on which side the details box pops up on

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
 
 	$(".iPhone").append('<div class="time">' + hours + ":" + minutes + " " + suffix + '</div>');
 
-	$(".expandable_trigger").click(function(){
-		$(".expandable").toggle();
-	});
+	$(".expandable_trigger").click(showProjectDetails);
 });
 
 var currentTime = new Date()
@@ -23,8 +21,20 @@ hours = 12;
 if (minutes < 10)
 minutes = "0" + minutes
 
+var showProjectDetails = function(e) {
+  var winWidth = $(window).width(),
+      $details = $(this).find('.expandable'),
+      detailsWidth = $details.width();
+     
+  $details.toggle();
+  var detailsOffsetX = $details.offset().left;
 
+  if ((detailsOffsetX + detailsWidth) > winWidth ) {
+    $details.toggleClass('tooltip_left');
+    $details.toggleClass('tooltip_right');
+    $details.css({'left': '-300px'});
+  }
 
-
-
+  return false;
+}
 
